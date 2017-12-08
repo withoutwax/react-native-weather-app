80453ab662508524fe26aa7599999da113d784e3
Will Kim <rlagmlckd@gmail.com>
2017-12-08 17:04:10 -0600

    Initial Commit

---
+++ components/SearchInput.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+
+export default class SearchInput extends React.Component {
+  render() {
+    return(
+      <View>
+        <TextInput
+          autoCorrect={false}
+          placeholder={this.props.placeholder}
+          placeholderTextColor="white"
+          style={styles.textInput}
+          clearButtonMode="always"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 40,
+    marginTop: 20,
+    backgroundColor: '#666',
+    marginHorizontal: 40,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  textInput: {
+    flex: 1,
+    color: 'white',
+  },
+});
